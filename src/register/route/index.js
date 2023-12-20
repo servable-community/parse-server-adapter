a8d6d40bd6c5ef1054213d3e6b09d37b18a93ef2
Aboubacar Doucoure <yelounak@gmail.com>
2023-12-20 17:27:32 +0100

    feat(module): added return to route handler

--- src/register/route/index.js
+++ src/register/route/index.js
@@ -12,6 +12,7 @@ export default ({ servableEngineConfig }) => {
       schema,
       handler,
       preHandler,
+      request = {}
       // rateLimiter = {
       //   windowMs = 60 * 60 * 1000, // 1 hour
       //   max = 1000, // Limit each IP to 5 create account requests per `window` (here, per hour)
@@ -41,20 +42,36 @@ export default ({ servableEngineConfig }) => {
             standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
             legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
           }),
-          async (request, response) => {
-            await handler(request, response)
+          async (request, response, next) => {
+            await doHandle({ handler, request, response, next })
           })
       } break
       case 'post': {
         Servable.AppNative.post(
           url,
-          bodyParser.raw({ type: 'application/json' }),
-          async (request, response) => {
-            await handler(request, response)
+          bodyParser.raw({ type: request.type ? request.type : 'application/json' }),
+          async (request, response, next) => {
+            await doHandle({ handler, request, response, next })
           })
       } break
       default: break
     }
   }
   return item
+}
+
+
+const doHandle = async ({ handler, request, response, next }) => {
+  try {
+    const result = await handler(request, response, next)
+    if (result) {
+      response.status(200).send(result)
+      // response.send(result)
+    }
+  } catch (e) {
+    const { message = "An error occurred", code = 500 } = e
+    // response.status(code).send(message)
+    // response.status(code)
+    next(e)
+  }
 }
